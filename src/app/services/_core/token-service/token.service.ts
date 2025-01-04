3dc9b603231adf33ab99aeeb389e8b44556bd8bc
Dai Hung PHAM <daihung.pham@gmail.com>
2025-01-05 00:43:06 +0100

    a

--- src/app/services/_core/token-service/token.service.ts
+++ src/app/services/_core/token-service/token.service.ts
@@ -9,49 +9,40 @@ import { environment } from '../../../../../environments/environment';
 })
 export class TokenService {
     private apiUrl = `${environment.urlBackend}/api`;
-    private idToken: string | null = null;
-    private refreshToken: string | null = null;
+    private idToken: string | null = null; // Stored in memory
 
     constructor(private http: HttpClient) { }
 
-    // Get the access token
+    // Get the ID Token
     getIdToken(): string | null {
         return this.idToken;
     }
 
-    getRefreshToken(): string | null {
-        return this.refreshToken;
-    }
-
-    // Set the access token
+    // Set the ID Token
     setIdToken(token: string): void {
         this.idToken = token;
     }
 
-    setRefreshToken(token: string): void {
-        this.refreshToken = token;
-    }
-
-    // Clear the access token
+    // Clear the ID Token
     clearIdToken(): void {
         this.idToken = null;
     }
 
-    // Refresh the access token using the refresh token (HttpOnly cookies)
+    // Refresh the ID Token using HttpOnly cookie
     refreshIdToken(): Observable<string> {
-        return this.http.get<{ refreshToken: string }>(
+        return this.http.get<{ idToken: string }>(
             `${this.apiUrl}/auth/refreshtoken`, 
-            {},
+            { withCredentials: true } // Ensure cookies are sent with the request
         ).pipe(
             tap((response: any) => {
-                this.setIdToken(response.idToken); // Save the new access token
+                this.setIdToken(response.idToken); // Update the ID Token in memory
             }),
             catchError((error) => {
                 this.clearIdToken();
+                console.error('❌ Failed to refresh token:', error);
                 return throwError(() => new Error('Failed to refresh access token'));
             }),
             map(response => response.idToken)
         );
     }
-    
 }
