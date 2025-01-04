9c76042c5319b7d244ea3d55d53ae77f15e3a80e
Dai Hung PHAM <daihung.pham@gmail.com>
2025-01-04 19:58:37 +0100

    a

--- environments/environment.ts
+++ environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: true,
   appId: 'aianalist', // Add your Parse app ID here
   version: `0.0.1`,
-  urlBackend: 'http://localhost:4444',
+  urlBackend: 'http://localhost:3333',
 };
 console.log("Environment Not Loaded");
